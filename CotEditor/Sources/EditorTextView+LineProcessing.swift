3f4c03e6fe606d00773dd5cdb8ec725d33d22110
1024jp <1024jp@wolfrosch.com>
2018-07-07 16:37:34 +0900

    Omit Nib.Name()

--- CotEditor/Sources/EditorTextView+LineProcessing.swift
+++ CotEditor/Sources/EditorTextView+LineProcessing.swift
@@ -195,7 +195,7 @@ extension EditorTextView {
     /// show pattern sort sheet
     @IBAction func patternSort(_ sender: Any?) {
         
-        let viewController = NSStoryboard(name: NSStoryboard.Name("PatternSortView"), bundle: nil).instantiateInitialController() as! PatternSortViewController
+        let viewController = NSStoryboard(name: "PatternSortView", bundle: nil).instantiateInitialController() as! PatternSortViewController
         viewController.representedObject = self
         
         // sample the first line
